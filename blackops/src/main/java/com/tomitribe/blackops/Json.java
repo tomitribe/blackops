67fe8938a7f1c257cc1f0f02558baa6fb1df4c6b
dblevins <david.blevins@gmail.com>
2015-07-28 17:11:01 -0700

    Write requests as well

--- blackops/src/main/java/com/tomitribe/blackops/Json.java
+++ blackops/src/main/java/com/tomitribe/blackops/Json.java
@@ -12,6 +12,7 @@ package com.tomitribe.blackops;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.tomitribe.util.PrintString;
 
 import java.io.IOException;
@@ -25,20 +26,33 @@ public class Json {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, false);
+        mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
 
         try {
             final PrintString out = new PrintString();
             mapper.writerWithDefaultPrettyPrinter().writeValue(out, object);
-            return out.toString();
+            return scrub(out.toString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * Not sure of another way to prevent Jackson from writing this element
+     *
+     * Neither WRITE_NULL_MAP_VALUES nor WRITE_EMPTY_JSON_ARRAYS seem to work
+     */
+    private static String scrub(final String string) {
+        return string.replace("  \"progressListener\" : { },", "");
     }
 
     public static <T> T fromString(final String json, final Class<T> clazz) {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
         try {
             return mapper.reader().forType(clazz).readValue(json.getBytes());
---
+++ blackops/src/test/java/com/tomitribe/blackops/JsonTest.java
@@ -0,0 +1,55 @@
+/*
+ * Tomitribe Confidential
+ *
+ * Copyright Tomitribe Corporation. 2015
+ *
+ * The source code for this program is not published or otherwise divested
+ * of its trade secrets, irrespective of what has been deposited with the
+ * U.S. Copyright Office.
+ */
+package com.tomitribe.blackops;
+
+import com.amazonaws.services.ec2.model.LaunchSpecification;
+import com.amazonaws.services.ec2.model.RequestSpotInstancesRequest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JsonTest extends Assert {
+
+    @Test
+    public void test() throws Exception {
+
+        final RequestSpotInstancesRequest request = new RequestSpotInstancesRequest()
+            .withSpotPrice("0.02")
+            .withInstanceCount(1)
+            .withType("one-time")
+            .withAvailabilityZoneGroup("us-east-1c")
+            .withLaunchSpecification(new LaunchSpecification()
+                .withInstanceType("m3.medium")
+                .withImageId("ami-5bf23530") // Operative 1.0
+                .withMonitoringEnabled(false)
+                .withKeyName("tomitribe_dev")
+                .withSecurityGroups("Ports 60000+10"));
+
+        final String json = Json.toString(request);
+        System.out.println(json);
+
+        assertContains(json, request.getSpotPrice());
+        assertContains(json, request.getInstanceCount() + "");
+        assertContains(json, request.getType());
+        assertContains(json, request.getAvailabilityZoneGroup());
+        assertContains(json, request.getLaunchSpecification().getInstanceType());
+        assertContains(json, request.getLaunchSpecification().getImageId());
+        assertContains(json, request.getLaunchSpecification().getKeyName());
+        assertContains(json, request.getLaunchSpecification().getSecurityGroups().get(0));
+
+        final RequestSpotInstancesRequest rehydrated = Json.fromString(json, RequestSpotInstancesRequest.class);
+        final String rewritten = Json.toString(rehydrated);
+
+        assertEquals(json, rewritten);
+    }
+
+    private void assertContains(String json, String value) {
+        assertTrue(String.format("Does not contain '%s'", value), json.contains(value));
+    }
+}
