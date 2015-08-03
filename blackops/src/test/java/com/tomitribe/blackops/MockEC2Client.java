0784e446f463d4dd8f3c1b9164fcc4440fb71242
dblevins <david.blevins@gmail.com>
2015-08-02 21:25:48 -0700

    Evaluate responses

--- blackops/src/test/java/com/tomitribe/blackops/MockEC2Client.java
+++ blackops/src/test/java/com/tomitribe/blackops/MockEC2Client.java
@@ -10,35 +10,77 @@
 package com.tomitribe.blackops;
 
 import com.amazonaws.services.ec2.AmazonEC2;
+import org.junit.Assert;
 import org.tomitribe.util.IO;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class MockEC2Client {
 
-    public static AmazonEC2 create(InvocationHandler h) {
+    private static final Pattern userDataMask = Pattern.compile("\"userData\" : \"([^\"]+)\"");
+    private static final Pattern operationIdMask = Pattern.compile("\"op-([^\"]+)\"");
+
+    public static AmazonEC2 create(final InvocationHandler h) {
         final ClassLoader loader = Thread.currentThread().getContextClassLoader();
         final Class[] interfaces = {AmazonEC2.class};
 
         return (AmazonEC2) Proxy.newProxyInstance(loader, interfaces, h);
     }
 
-    public static AmazonEC2 create(final Object... responses) {
-        final Iterator<Object> iterator = Arrays.asList(responses).iterator();
-        return create((proxy, method, args) -> iterator.next());
+    public static AmazonEC2 create(final Object... objects) {
+        final Iterator<Object> iterator = Arrays.asList(objects).iterator();
+        return create(new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (args != null && args.length == 1) {
+                    compare(iterator.next(), args[0]);
+                }
+                if (Void.TYPE.equals(method.getReturnType())) {
+                    return null;
+                }
+                return iterator.next();
+            }
+        });
+    }
+
+    private static void compare(final Object expected, final Object actual) {
+        final String jsonA = normalize(Json.toString(expected));
+        final String jsonB = normalize(Json.toString(actual));
+        Assert.assertEquals(jsonA, jsonB);
+    }
+
+
+    private static String normalize(String json) {
+        json = mask(json, operationIdMask);
+        json = mask(json, userDataMask);
+        return json;
+    }
+
+    private static String mask(String json, Pattern compile) {
+        final Matcher m = compile.matcher(json);
+        if (m.find()) {
+            final String data = m.group(1);
+            final String mask = data.replaceAll(".", "x");
+            json = json.replace(data, mask);
+        }
+
+        return json;
     }
 
     public static AmazonEC2 fromJson(String... resources) throws ClassNotFoundException, IOException {
-        return create(Stream.of(resources).map(MockEC2Client::loadResponse).toArray());
+        return create(Stream.of(resources).map(MockEC2Client::load).toArray());
     }
 
-    public static Object loadResponse(String name) {
+    public static Object load(String name) {
         final ClassLoader classLoader = MockEC2Client.class.getClassLoader();
         try {
             final String type = name.split("-")[1];
